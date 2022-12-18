d81b6c399eb6789c8fcb1386f9b759a6c28bec95
syncclk <37027395+syncclk@users.noreply.github.com>
2022-12-18 12:09:19 -0800

    Minimum freestanding Rust binary.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+// main.rs
+#![no_std]   // We don't want to link to the standard library.
+#![no_main]  // We want to override crt0 entry point
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic (_info : &PanicInfo) -> ! {
+    loop {}
 }
